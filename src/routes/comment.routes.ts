4267a621457cb34040741cdd42414f2003af0fa0
MrPeaceDuke <daemonica120@yandex.ru>
2025-05-14 22:54:04 +0500

    fix routes access

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import * as CommentController from '../controllers/comment.controller';
+import { authenticate, authorize } from "../middlewares/auth.middleware";
+import * as CommentController from "../controllers/comment.controller";
+import { Role } from "../types/role";
 const router = Router();
 
-router.post("/create", CommentController.create);
+router.post(
+  "/create",
+  authenticate,
+  authorize([Role.USER, Role.ADMIN]),
+  CommentController.create
+);
 router.post("/getByPostId", CommentController.getByPostId);
 
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
-import * as UserController from '../controllers/user.controller';
+import * as UserController from "../controllers/user.controller";
+import { authenticate, authorize } from "../middlewares/auth.middleware";
+import { Role } from "../types/role";
 const router = Router();
 
-router.post("/ban", UserController.ban);
+router.post(
+  "/ban",
+  authenticate,
+  authorize([Role.ADMIN]),
+  UserController.ban
+);
 
 export default router;
